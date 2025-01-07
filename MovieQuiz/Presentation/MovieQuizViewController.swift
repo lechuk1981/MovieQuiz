2377a63103d7b12ec509ff336d202acd00632d73
Andrey Sopov <lechuk@yandex.ru>
2025-01-08 00:16:32 +0300

    Final

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -7,23 +7,6 @@ final class MovieQuizViewController: UIViewController {
     @IBOutlet private var imageView: UIImageView!
     @IBOutlet private var counterLabel: UILabel!
     
-    @IBAction private func yesButtonClicked(_ sender: UIButton) {
-        
-        let currentQuestion = questions[currentQuestionIndex]
-        let givenAnswer = true
-        
-        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
-    }
-    
-    @IBAction private func noButtonClicked(_ sender: UIButton) {
-        
-        let currentQuestion = questions[currentQuestionIndex]
-        let givenAnswer = false
-        
-        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
-        
-    }
-    
     struct QuizQuestion {
         let image: String
         let text: String
@@ -42,7 +25,44 @@ final class MovieQuizViewController: UIViewController {
         let buttonText: String
     }
     
+    
+    private let questions: [QuizQuestion] = [
+        .init(image: "The Godfather", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+        .init(image: "The Dark Knight", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+        .init(image: "Kill Bill", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+        .init(image: "The Avengers", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+        .init(image: "Deadpool", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+        .init(image: "The Green Knight", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+        .init(image: "Old", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
+        .init(image: "The Ice Age Adventures of Buck Wild", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
+        .init(image: "Tesla", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
+        .init(image: "Vivarium", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
+    ]
+    
+    
+    private var correctAnswers = 0
+    private var currentQuestionIndex = 0
+    
+    @IBAction private func yesButtonClicked(_ sender: UIButton) {
+        
+        let currentQuestion = questions[currentQuestionIndex]
+        let givenAnswer = true
+        
+        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+    }
+    
+    @IBAction private func noButtonClicked(_ sender: UIButton) {
+        
+        let currentQuestion = questions[currentQuestionIndex]
+        let givenAnswer = false
+        
+        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+        
+    }
+    
+       
     private func show(quiz result: QuizResultsViewModel) {
+        
         let alert = UIAlertController(
             title: result.title,
             message: result.text,
@@ -62,21 +82,6 @@ final class MovieQuizViewController: UIViewController {
         present(alert, animated: true)
     }
     
-    private let questions: [QuizQuestion] = [
-        .init(image: "The Godfather", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
-        .init(image: "The Dark Knight", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
-        .init(image: "Kill Bill", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
-        .init(image: "The Avengers", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
-        .init(image: "Deadpool", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
-        .init(image: "The Green Knight", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
-        .init(image: "Old", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
-        .init(image: "The Ice Age Adventures of Buck Wild", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
-        .init(image: "Tesla", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
-        .init(image: "Vivarium", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
-    ]
-    
-    private var correctAnswers = 0
-    private var currentQuestionIndex = 0
     
     override func viewDidLoad() {
         
