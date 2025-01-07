caac2335600e6456a1b6b34d47ef41fc592ef7a2
Andrey Sopov <lechuk@yandex.ru>
2025-01-08 00:24:28 +0300

    Remove sleep

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -7,7 +7,6 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
         
-        sleep(3)
         
         return true
     }
